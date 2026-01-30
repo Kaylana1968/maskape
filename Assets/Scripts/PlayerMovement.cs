e218398865b41cdab0bb8eb8fbdd7d153c27f4a9
TheStarBars <aoges@orange.fr>
2026-01-31 00:28:41 +0100

    ADD: mask color init

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,49 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Réglages")]
     public float speed = 5f;
+    private InputAction action;
     public float mouseSensitivity = 20f;
     public Transform cameraTransform; // Glisse la Main Camera ici
-
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector2 lookInput;
     private float xRotation = 0f;
-
+    private string[] listColor = new string[] {"Red", "Blue", "Green"};
+    private string currentColor;
     void Awake()
     {
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Start()
+    {
+        currentColor = "Red";
+        GameObject[] gameObjectsRed = GameObject.FindGameObjectsWithTag("Red");
+        GameObject[] gameObjectsBlue = GameObject.FindGameObjectsWithTag("Blue");
+        GameObject[] gameObjectsGreen = GameObject.FindGameObjectsWithTag("Green");
+        GameObject Mask = GameObject.FindGameObjectWithTag("Mask");
+
+        Mask.gameObject.GetComponent<Image>().material.color = new Color(255,0,0);
+        foreach (GameObject gameObject in gameObjectsBlue)
+        {
+            gameObject.SetActive(false);
+        }
+        foreach (GameObject gameObject in gameObjectsGreen)
+        {
+            gameObject.SetActive(false);
+        }
+        foreach (GameObject gameObject in gameObjectsRed)
+        {
+            gameObject.SetActive(true);
+        }
     }
 
     // Reçoit l'input de déplacement (Z,Q,S,D)
